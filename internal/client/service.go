61bd62e82d3ebca59c503014696e075d1366acce
isarasibar <sarasibar.ignacio@gmail.com>
2022-10-23 22:55:32 -0300

    Fix Get Quests

--- internal/client/service.go
+++ internal/client/service.go
@@ -75,6 +75,8 @@ func (s *service) GetClientQuests(c *gin.Context, clientID int) ([]domain.QuestI
 		questsDTO[i].Description = quests[i].Description
 		questsDTO[i].Difficulty = quests[i].Difficulty
 		questsDTO[i].Duration = quests[i].Duration
+		questsDTO[i].Image = quests[i].Image
+		questsDTO[i].Completions = quests[i].Completions
 
 		tags, err := s.repo.GetTags(c, id)
 		if err != nil {
