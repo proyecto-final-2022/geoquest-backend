9ce4698665b53ee8d52db5112e66ce261b97256c
isarasibar <sarasibar.ignacio@gmail.com>
2022-10-26 23:20:08 -0300

    Changed Connection String

--- config/db.go
+++ config/db.go
@@ -15,7 +15,7 @@ var Mongo *mongo.Client
 
 func Connect() {
 	//TODO: poner esto en archivo de configuracion
-	mySqlConn := "geoquest:geoquest@tcp(localhost:3306)/geoquest?charset=utf8mb4&parseTime=True&loc=Local"
+	mySqlConn := "geoquest:geoquest@tcp(test1-geoquest.c6pcyhhnplzm.us-east-1.rds.amazonaws.com:3306)/geoquest?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(mySqlConn), &gorm.Config{})
 
 	if err != nil {
