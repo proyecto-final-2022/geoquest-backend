d06159c130f841eb9b2d109c3926d391f77e20e2
PabloOve <31904880+PabloOve@users.noreply.github.com>
2022-10-29 15:24:09 -0300

    client coupons

--- internal/domain/client.go
+++ internal/domain/client.go
@@ -1,10 +1,11 @@
 package domain
 
 type Client struct {
-	ID    int         `json:"id,identity" gorm:"primary_key"`
-	Name  string      `json:"name"`
-	Image string      `json:"image"`
-	Quest []QuestInfo `json:"quests" gorm:"foreignKey:ClientID;references:ID"`
+	ID     int         `json:"id,identity" gorm:"primary_key"`
+	Name   string      `json:"name"`
+	Image  string      `json:"image"`
+	Quest  []QuestInfo `json:"quests" gorm:"foreignKey:ClientID;references:ID"`
+	Coupon []Coupon    `json:"coupons" gorm:"foreignKey:ClientID;references:ID"`
 }
 
 type ClientDTO struct {
